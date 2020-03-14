c8b29365dc1c4b76caf013efd1c27c58f71a8eed
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-03-14 14:20:14 -0700

    Added Jazz, Urban/R&B, Pop and Rock/Metal/Punk genres.

--- src/genres.js
+++ src/genres.js
@@ -2,4 +2,8 @@ export default {
     NO_GENRE: 'No Genre',
     ELECTRONIC: 'Electronic',
     HIPHOP: 'Hip-Hop/Rap',
+    JAZZ: 'Jazz',
+    POP: 'Pop',
+    ROCK_METAL_PUNK: 'Rock/Metal/Punk',
+    URBAN_RNB: 'Urban/R&B',
 };
