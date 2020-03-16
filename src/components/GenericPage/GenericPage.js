385b7419da099ec2617debb10099d0099aa1dc87
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-03-16 16:51:23 -0700

    Dynamically adjust Sider width to allow 560px of regular content at minimum (unless the Sider would be <200px or >256px). Menu icons match the font size of the Menu.Item itself.

--- src/components/GenericPage/GenericPage.js
+++ src/components/GenericPage/GenericPage.js
@@ -46,6 +46,19 @@ class GenericPage extends React.Component<Props> {
         });
     };
 
+    getSiderWidth = () => {
+        // Ideally, 560px+ of content should be visible besides the Sider.
+        // A Sider of <200px is awkward though and similarly, a Sider of >256px
+        // is redundant.
+        let siderWidth = window.screen.width - 560;
+        if (siderWidth < 200) {
+            siderWidth = 200;
+        } else if (siderWidth > 256) {
+            siderWidth = 256;
+        }
+        return siderWidth + 'px';
+    };
+
     render() {
         return (
             <Layout className="page-container">
@@ -53,7 +66,7 @@ class GenericPage extends React.Component<Props> {
                     {!this.props.hideMenu && this.props.isMobile && <MenuBar isMobile={this.props.isMobile} />}
                     <Layout.Content>
                         <Layout>
-                            {!this.props.hideMenu && !this.props.isMobile && <Layout.Sider theme="light" width="256px">
+                            {!this.props.hideMenu && !this.props.isMobile && <Layout.Sider theme="light" width={this.getSiderWidth()}>
                                 <MenuBar />
                             </Layout.Sider>}
                             <Layout.Content className="page-content">
