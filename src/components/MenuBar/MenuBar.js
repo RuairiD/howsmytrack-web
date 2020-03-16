d0fc81b3e2177af06f1d2b37de4ef209913e5af1
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-03-16 15:46:46 -0700

    Refactored MenuBar to use helper components instead of `this.renderXyz` functions.

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -33,6 +33,283 @@ const USER_DETAILS_QUERY = `query UserDetails {
 
 const GA_MENU_BAR_CATEGORY = "menubar";
 
+const UserDetails = ({ username, rating }) => {
+    if (rating) {
+        return (
+            <Menu.Item disabled className="ant-menu-item">
+                <div style={{ display: 'flex' }}>
+                    <Typography.Text ellipsis style={{
+                        marginRight: 'auto',
+                        marginTop: 'auto',
+                        marginBottom: 'auto',
+                    }}>
+                        {username}
+                    </Typography.Text>
+                    <Typography.Text strong style={{
+                        marginLeft: 'auto',
+                        marginTop: 'auto',
+                        marginBottom: 'auto',
+                    }}>
+                        {rating.toFixed(2)}<Icon type="star" />
+                    </Typography.Text>
+                </div>
+            </Menu.Item>
+        )
+    }
+    return (
+        <Menu.Item disabled className="ant-menu-item">
+            <div style={{ display: 'flex' }}>
+                <Typography.Text ellipsis style={{
+                    marginRight: 'auto',
+                    marginTop: 'auto',
+                    marginBottom: 'auto',
+                }}>
+                    {username}
+                </Typography.Text>
+            </div>
+        </Menu.Item>
+    );
+};
+
+const SocialLinksMenu = ({ isMobile, onMenuClick }) => {
+    let style = {};
+    if (!isMobile) {
+        style = {
+            position: 'absolute',
+            bottom: '0px',
+        };
+    }
+    return (
+        <div style={style}>
+            <Menu
+                theme="light"
+                onClick={onMenuClick}
+                style={{
+                    borderRight: 0,
+                }}
+            >
+                {isMobile && <Menu.Divider />}
+                <Menu.Item className="ant-menu-item">
+                    <a href="https://twitter.com/howsmytrackcom" target="_blank" rel="noopener noreferrer">
+                        <Icon type="twitter" />
+                        <span>Twitter</span>
+                    </a>
+                </Menu.Item>
+                <Menu.Item className="ant-menu-item">
+                    <a href="https://discord.gg/6jGFP6N" target="_blank" rel="noopener noreferrer">
+                        <Icon type="robot" />
+                        <span>Discord</span>
+                    </a>
+                </Menu.Item>
+            </Menu>
+        </div>
+    );
+};
+
+const MobileHeader = ({ mobileMenuCollapsed, notifications }) => (
+    <div style={{ display: 'flex' }}>
+        <span style={{
+            marginTop: 'auto',
+            marginBottom: 'auto',
+        }}>
+            <Typography.Text>
+                how's my track?
+            </Typography.Text>
+            {mobileMenuCollapsed && <Badge
+                count={notifications}
+                style={{
+                    marginLeft: '0.5em',
+                }}
+            />}
+        </span>
+        <img
+            alt=""
+            src="/logo128.png"
+            style={{
+                marginLeft: 'auto',
+                width: '32px',
+                height: '32px',
+            }}
+        />
+    </div>
+);
+
+const LoggedOutMenu = ({ onMenuClick }) => (
+    <Menu
+        theme="light"
+        onClick={onMenuClick}
+        style={{
+            borderRight: 0,
+        }}
+    >
+        <Menu.Item disabled>
+            <Typography.Text>Welcome!</Typography.Text>
+        </Menu.Item>
+        <Menu.Item key="login">
+            <Icon type="user" />
+            <span>Sign In</span>
+        </Menu.Item>
+        <Menu.Item key="register">
+            <Icon type="user-add" />
+            <span>Register</span>
+        </Menu.Item>
+        <Menu.Item key="faq">
+            <a href="/faq">
+                <Icon type="question-circle" />
+                <span>FAQ</span>
+            </a>
+        </Menu.Item>
+    </Menu>
+);
+
+const LoggedInMenu = ({ onMenuClick, username, rating, notifications }) => (
+    <Menu
+        theme="light"
+        onClick={onMenuClick}
+        style={{
+            marginBottom: 'auto',
+            borderRight: 0,
+        }}
+    >
+        <UserDetails
+            username={username}
+            rating={rating}
+        />
+        <Menu.Item key="newRequest">
+            <Icon type="plus" />
+            <span>New Request</span>
+        </Menu.Item>
+        <Menu.Item key="feedbackGroups">
+            <a href="/groups">
+                <Icon type="team" />
+                <span>Your Groups</span>
+                <Badge
+                    count={notifications}
+                    style={{ marginLeft: '0.5em' }}
+                />
+            </a>
+        </Menu.Item>
+        <Menu.Item key="faq">
+            <a href="/faq">
+                <Icon type="question-circle" />
+                <span>FAQ</span>
+            </a>
+        </Menu.Item>
+        <Menu.Item key="logout">
+            <Icon type="user-delete" />
+            <span>Sign Out</span>
+        </Menu.Item>
+    </Menu>
+);
+
+const MainMenu = ({
+    isMobile,
+    onMenuClick,
+    username,
+    rating,
+    notifications,
+}) => (
+    <div>
+        {username && <LoggedInMenu
+            onMenuClick={onMenuClick}
+            username={username}
+            rating={rating}
+            notifications={notifications}
+        />}
+        {!username && <LoggedOutMenu
+            onMenuClick={onMenuClick}
+        />}
+        <SocialLinksMenu
+            isMobile={isMobile}
+            onMenuClick={onMenuClick}
+        />
+    </div>
+);
+
+const MobileMenu = ({
+    isMobile,
+    onMenuClick,
+    username,
+    rating,
+    notifications,
+    onCollapseChange,
+    mobileMenuCollapsed,
+}) => (
+    <Collapse
+        className="mobile-menu-collapse"
+        bordered={false}
+        expandIcon={
+            ({ isActive }) => (
+                <Icon type="menu" rotate={isActive ? 90 : 0} />
+            )
+        }
+        onChange={onCollapseChange}
+    >
+        <Collapse.Panel
+            header={(
+                <MobileHeader
+                    mobileMenuCollapsed={mobileMenuCollapsed}
+                    notifications={notifications}
+                />
+            )}
+            key="1"
+        >
+            <MainMenu
+                isMobile={isMobile}
+                onMenuClick={onMenuClick}
+                username={username}
+                rating={rating}
+                notifications={notifications}
+            />
+        </Collapse.Panel>
+    </Collapse>
+);
+
+const MenuBarContent = ({
+    hasProps,
+    isMobile,
+    onMenuClick,
+    username,
+    rating,
+    notifications,
+    isFeedbackRequestModalVisible,
+    onFeedbackRequestModalCancel,
+    isLoginModalVisible,
+    onLoginModalCancel,
+    isRegisterModalVisible,
+    onRegisterModalCancel,
+    mobileMenuCollapsed,
+    onCollapseChange,
+}) => (
+    <Spin spinning={!hasProps}>
+        <div className="menu-bar">
+            {!isMobile && <a href="/" className="full-logo">
+                <img alt="how's my track" src="/logo512.png" width="200px" style={{ padding: '0.5em'}} />
+            </a>}
+            {isMobile && <MobileMenu
+                isMobile={isMobile}
+                onMenuClick={onMenuClick}
+                username={username}
+                rating={rating}
+                notifications={notifications}
+                onCollapseChange={onCollapseChange}
+                mobileMenuCollapsed={mobileMenuCollapsed}
+            />}
+            {!isMobile && <MainMenu
+                isMobile={isMobile}
+                onMenuClick={onMenuClick}
+                username={username}
+                rating={rating}
+                notifications={notifications}
+            />}
+
+            <FeedbackRequestModal onCancel={onFeedbackRequestModalCancel} isVisible={isFeedbackRequestModalVisible} />
+            <LoginModal onCancel={onLoginModalCancel} isVisible={isLoginModalVisible} />
+            <RegisterModal onCancel={onRegisterModalCancel} isVisible={isRegisterModalVisible} />
+        </div>
+    </Spin>
+);
+
 class MenuBar extends React.Component<Props, State> {
     /*
      * Component for displaying page sidebar with menu links, or for mobile,
@@ -109,13 +386,14 @@ class MenuBar extends React.Component<Props, State> {
             })
         },
         logout: () => {
-            this.logout().then(result =>
+            this.logout().then(() =>
                 window.location.assign('/')
             );
         },
     };
 
     onMenuClick = (event) => {
+        console.log(event.key);
         let menuAction = this.menuActions[event.key];
         ReactGA.event({
             category: GA_MENU_BAR_CATEGORY,
@@ -144,181 +422,6 @@ class MenuBar extends React.Component<Props, State> {
         })
     };
 
-    renderMobileHeader = () => {
-        return (
-            <div style={{ display: 'flex' }}>
-                <span style={{
-                    marginTop: 'auto',
-                    marginBottom: 'auto',
-                }}>
-                    <Typography.Text>
-                        how's my track?
-                    </Typography.Text>
-                    {this.state.mobileMenuCollapsed && <Badge
-                        count={this.state.notifications}
-                        style={{
-                            marginLeft: '0.5em',
-                        }}
-                    />}
-                </span>
-                <img
-                    alt=""
-                    src="/logo128.png"
-                    style={{
-                        marginLeft: 'auto',
-                        width: '32px',
-                        height: '32px',
-                    }}
-                />
-            </div>
-        )
-    };
-
-    renderSocialLinks = () => {
-        return (
-            <Menu
-                theme="light"
-                onClick={this.onMenuClick}
-                style={{
-                    borderRight: 0,
-                }}
-            >
-                {this.props.isMobile && <Menu.Divider />}
-                <Menu.Item className="ant-menu-item">
-                    <a href="https://twitter.com/howsmytrackcom" target="_blank" rel="noopener noreferrer">
-                        <Icon type="twitter" />
-                        <span>Twitter</span>
-                    </a>
-                </Menu.Item>
-                <Menu.Item className="ant-menu-item">
-                    <a href="https://discord.gg/6jGFP6N" target="_blank" rel="noopener noreferrer">
-                        <Icon type="robot" />
-                        <span>Discord</span>
-                    </a>
-                </Menu.Item>
-            </Menu>
-        );
-    };
-
-    renderLoggedOutMenu = () => {
-        return (
-            <Spin spinning={!this.state.hasProps}>
-                <Menu
-                    theme="light"
-                    onClick={this.onMenuClick}
-                    style={{
-                        borderRight: 0,
-                    }}
-                >
-                    <Menu.Item disabled>
-                        <Typography.Text>Welcome!</Typography.Text>
-                    </Menu.Item>
-                    <Menu.Item key="login">
-                        <Icon type="user" />
-                        <span>Sign In</span>
-                    </Menu.Item>
-                    <Menu.Item key="register">
-                        <Icon type="user-add" />
-                        <span>Register</span>
-                    </Menu.Item>
-                    <Menu.Item key="faq">
-                        <a href="/faq">
-                            <Icon type="question-circle" />
-                            <span>FAQ</span>
-                        </a>
-                    </Menu.Item>
-                </Menu>
-                {this.renderSocialLinks()}
-            </Spin>
-        )
-    };
-
-    renderUserDetails = () => {
-        if (this.state.rating) {
-            return (
-                <Menu.Item disabled>
-                    <div style={{ display: 'flex' }}>
-                        <Typography.Text ellipsis style={{
-                            marginRight: 'auto',
-                            marginTop: 'auto',
-                            marginBottom: 'auto',
-                        }}>
-                            {this.state.username}
-                        </Typography.Text>
-                        <Typography.Text strong style={{
-                            marginLeft: 'auto',
-                            marginTop: 'auto',
-                            marginBottom: 'auto',
-                        }}>
-                            {this.state.rating.toFixed(2)}<Icon type="star" />
-                        </Typography.Text>
-                    </div>
-                </Menu.Item>
-            )
-        }
-        return (
-            <Menu.Item disabled>
-                <div style={{ display: 'flex' }}>
-                    <Typography.Text ellipsis style={{
-                        marginRight: 'auto',
-                        marginTop: 'auto',
-                        marginBottom: 'auto',
-                    }}>
-                        {this.state.username}
-                    </Typography.Text>
-                </div>
-            </Menu.Item>
-        )
-    };
-
-    renderLoggedInMenu = () => {
-        return (
-            <React.Fragment>
-                <Menu
-                    theme="light"
-                    onClick={this.onMenuClick}
-                    style={{
-                        marginBottom: 'auto',
-                        borderRight: 0,
-                    }}
-                >
-                    {this.renderUserDetails()}
-                    <Menu.Item key="newRequest">
-                        <Icon type="plus" />
-                        <span>New Request</span>
-                    </Menu.Item>
-                    <Menu.Item key="feedbackGroups">
-                        <a href="/groups">
-                            <Icon type="team" />
-                            <span>Your Groups</span>
-                            <Badge
-                                count={this.state.notifications}
-                                style={{ marginLeft: '0.5em' }}
-                            />
-                        </a>
-                    </Menu.Item>
-                    <Menu.Item key="faq">
-                        <a href="/faq">
-                            <Icon type="question-circle" />
-                            <span>FAQ</span>
-                        </a>
-                    </Menu.Item>
-                    <Menu.Item key="logout">
-                        <Icon type="user-delete" />
-                        <span>Sign Out</span>
-                    </Menu.Item>
-                </Menu>
-                {!this.props.isMobile && <div style={{
-                    position: 'absolute',
-                    bottom: '0px',
-                }}>
-                    {this.renderSocialLinks()}
-                </div>}
-                {this.props.isMobile && this.renderSocialLinks()}
-            </React.Fragment>
-        )
-    };
-
     onCollapseChange = (key) => {
         if (key.length > 0) {
             this.setState({
@@ -331,58 +434,44 @@ class MenuBar extends React.Component<Props, State> {
         }
     };
 
-    renderMobileMenu = () => {
-        return (
-            <Collapse
-                className="mobile-menu-collapse"
-                bordered={false}
-                expandIcon={
-                    ({ isActive }) => (
-                        <Icon type="menu" rotate={isActive ? 90 : 0} />
-                    )
-                }
-                onChange={this.onCollapseChange}
-            >
-                <Collapse.Panel
-                    header={this.renderMobileHeader()}
-                    key="1"
-                >
-                    {this.renderDefaultDeviceMenu()}
-                </Collapse.Panel>
-            </Collapse>
-        );
-    };
-
-    renderDefaultDeviceMenu = () => {
-        if (this.state.username) {
-            return this.renderLoggedInMenu();
-        }
-        return this.renderLoggedOutMenu();
-    };
-
-    renderMenuBarContent = () => {
-        return (
-            <div className="menu-bar">
-                {!this.props.isMobile && <a href="/">
-                    <img alt="how's my track" src="/logo512.png" width="200px" style={{ padding: '0.5em'}} />
-                </a>}
-                {this.props.isMobile && this.renderMobileMenu()}
-                {!this.props.isMobile && this.renderDefaultDeviceMenu()}
-
-                <FeedbackRequestModal onCancel={this.onFeedbackRequestModalCancel} isVisible={this.state.isFeedbackRequestModalVisible} />
-                <LoginModal onCancel={this.onLoginModalCancel} isVisible={this.state.isLoginModalVisible} />
-                <RegisterModal onCancel={this.onRegisterModalCancel} isVisible={this.state.isRegisterModalVisible} />
-            </div>
-        );
-    };
-
     render() {
         if (this.props.isMobile) {
-            return this.renderMenuBarContent();
+            return (
+                <MenuBarContent
+                    isMobile={this.props.isMobile}
+                    hasProps={this.state.hasProps}
+                    onMenuClick={this.onMenuClick}
+                    username={this.state.username}
+                    rating={this.state.rating}
+                    notifications={this.state.notifications}
+                    isFeedbackRequestModalVisible={this.state.isFeedbackRequestModalVisible}
+                    onFeedbackRequestModalCancel={this.onFeedbackRequestModalCancel}
+                    isLoginModalVisible={this.state.isLoginModalVisible}
+                    onLoginModalCancel={this.onLoginModalCancel}
+                    isRegisterModalVisible={this.state.isRegisterModalVisible}
+                    onRegisterModalCancel={this.onRegisterModalCancel}
+                    onCollapseChange={this.onCollapseChange}
+                />
+            );
         }
         return (
             <Affix>
-                {this.renderMenuBarContent()}
+                <MenuBarContent
+                    isMobile={this.props.isMobile}
+                    hasProps={this.state.hasProps}
+                    onMenuClick={this.onMenuClick}
+                    username={this.state.username}
+                    rating={this.state.rating}
+                    notifications={this.state.notifications}
+                    isFeedbackRequestModalVisible={this.state.isFeedbackRequestModalVisible}
+                    onFeedbackRequestModalCancel={this.onFeedbackRequestModalCancel}
+                    isLoginModalVisible={this.state.isLoginModalVisible}
+                    onLoginModalCancel={this.onLoginModalCancel}
+                    isRegisterModalVisible={this.state.isRegisterModalVisible}
+                    onRegisterModalCancel={this.onRegisterModalCancel}
+                    mobileMenuCollapsed={this.state.mobileMenuCollapsed}
+                    onCollapseChange={this.onCollapseChange}
+                />
             </Affix>
         );
     }
