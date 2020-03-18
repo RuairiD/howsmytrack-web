ad1b4d30fdf870da18c16834787edd59760ecce1
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-03-18 16:42:05 -0700

    Fetch userDetails for UserSettingsPage. Make updateEmail and updateSendReminderEmails requests when user updates values in settings.

--- src/components/UserSettingsPage/UserSettingsPage.js
+++ src/components/UserSettingsPage/UserSettingsPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import apiRoot from '../../apiRoot';
+
 import GenericPage from '../GenericPage/GenericPage';
 import UserSettings from '../UserSettings/UserSettings';
 
@@ -7,13 +9,53 @@ type Props = {
     isMobile: boolean,
 };
 
-class UserSettingsPage extends React.Component<Props> {
+type State = {
+    hasProps: boolean,
+    userDetails: UserSettingsProps,
+};
+
+const USER_DETAILS_QUERY = `query UserDetails {
+  userDetails {
+    username
+    sendReminderEmails
+  }
+}`;
+
+class UserSettingsPage extends React.Component<Props, State> {
+    state = {
+        hasProps: false,
+        userDetails: null,
+    }
+
+    componentDidMount() {
+        fetch(apiRoot +'/graphql/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                query: USER_DETAILS_QUERY,
+            }),
+            credentials: 'include',
+        }).then(result =>
+            result.json()
+        ).then((data) => {
+            if (data['data']['userDetails']) {
+                this.setState({
+                    hasProps: true,
+                    userDetails: data['data']['userDetails'],
+                });
+            };
+        });
+    }
+
     render() {
-        return (
+        return this.state.hasProps && (
             <GenericPage title="Settings" isMobile={this.props.isMobile}>
                 <UserSettings
-                    email="ruairidx@gmail.com"
-                    sendReminderEmails={true}
+                    email={this.state.userDetails.username}
+                    sendReminderEmails={this.state.userDetails.sendReminderEmails}
                 />
             </GenericPage>
         )
