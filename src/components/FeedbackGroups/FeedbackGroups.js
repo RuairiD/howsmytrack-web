29165b8418c7117d1cab641758bda508a4dee5c8
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-09-28 13:03:34 -0700

    All components that aren’t forms and don’t make network requests are functional components.

--- src/components/FeedbackGroups/FeedbackGroups.js
+++ src/components/FeedbackGroups/FeedbackGroups.js
@@ -11,67 +11,71 @@ type Props = {
     unassignedRequest: FeedbackRequestSummaryProps,
 };
 
-class FeedbackGroups extends React.Component<Props> {
+const AssignedGroups = ({ feedbackGroups }) => {
+    if (feedbackGroups && feedbackGroups.length > 0) {
+        return (
+            <List
+                grid={{
+                    gutter: 16,
+                    xs: 1,
+                    sm: 1,
+                    md: 1,
+                    lg: 1,
+                    xl: 2,
+                    xxl: 2,
+                }}
+                dataSource={feedbackGroups}
+                renderItem={feedbackGroup => (
+                    <List.Item style={{ width: '100%' }}>
+                        <FeedbackGroupPreview
+                            {...feedbackGroup}
+                        />
+                    </List.Item>
+                )}
+            />
+        );
+    }
+    return (
+        <Empty
+            style={{ color: '#444444' }}
+            image={Empty.PRESENTED_IMAGE_SIMPLE}
+            description="You haven't been assigned any groups yet. Perhaps you should make a request?"
+        />
+    );
+};
+
+const UnassignedRequest = ({ unassignedRequest }) => (
+    <React.Fragment>
+        <Row gutter={[16, 16]}>
+            <Col>
+                <Typography.Title level={3}>Your Unassigned Request</Typography.Title>
+                <FeedbackRequestSummary
+                    feedbackRequestSummary={unassignedRequest}
+                    showButtons
+                />
+            </Col>
+        </Row>
+        <Divider />
+    </React.Fragment>
+);
+
+const FeedbackGroups = ({
+    feedbackGroups,
+    unassignedRequest,
+}: Props) => {
     /*
      * Component for displaying user details/groups page showing user info and all groups they are a part of.
      */
-
-    renderFeedbackGroups = () => {
-        if (this.props.feedbackGroups && this.props.feedbackGroups.length > 0) {
-            return (
-                <List
-                    grid={{
-                        gutter: 16,
-                        xs: 1,
-                        sm: 1,
-                        md: 1,
-                        lg: 1,
-                        xl: 2,
-                        xxl: 2,
-                    }}
-                    dataSource={this.props.feedbackGroups}
-                    renderItem={feedbackGroup => (
-                        <List.Item style={{ width: '100%' }}>
-                            <FeedbackGroupPreview
-                                {...feedbackGroup}
-                            />
-                        </List.Item>
-                    )}
-                />
-            );
-        }
-        return (
-            <Empty
-                style={{ color: '#444444' }}
-                image={Empty.PRESENTED_IMAGE_SIMPLE}
-                description="You haven't been assigned any groups yet. Perhaps you should make a request?"
-            />
-        );
-    };
-
-    render() {
-        return (
-            <div>
-                {this.props.unassignedRequest && <React.Fragment>
-                    <Row gutter={[16, 16]}>
-                        <Col>
-                            <Typography.Title level={3}>Your Unassigned Request</Typography.Title>
-                            <FeedbackRequestSummary
-                                feedbackRequestSummary={this.props.unassignedRequest}
-                                showButtons
-                            />
-                        </Col>
-                    </Row>
-                    <Divider />
-                </React.Fragment>}
-                <Row gutter={[16, 16]}>
-                    <Col>
-                        {this.renderFeedbackGroups()}
-                    </Col>
-                </Row>
-            </div>
-        )
-    }
+    return (
+        <div>
+            {unassignedRequest && <UnassignedRequest unassignedRequest={unassignedRequest} />}
+            <Row gutter={[16, 16]}>
+                <Col>
+                    <AssignedGroups feedbackGroups={feedbackGroups} />
+                </Col>
+            </Row>
+        </div>
+    )
 }
 
 export default FeedbackGroups;
--- src/components/FeedbackRequestSummary/FeedbackRequestSummaryContent.js
+++ src/components/FeedbackRequestSummary/FeedbackRequestSummaryContent.js
@@ -5,64 +5,61 @@ import MediaEmbed from '../MediaEmbed/MediaEmbed';
 import GENRE_OPTIONS from '../../genres';
 import type { FeedbackRequestSummaryProps } from './FeedbackRequestSummary';
 
+const Media = ({ mediaUrl, mediaType }) => {
+    if (!mediaUrl) {
+        return (
+            <Alert
+                message="No track has been provided for this request. You'll be able to join a group and provide feedback for others, but you won't receive any feedback for yourself."
+                type="info"
+                showIcon
+            />
+        )
+    }
+    if (!mediaType) {
+        return (
+            <Alert
+                message="Unable to play media. Invalid URL."
+                type="error"
+                showIcon
+            />
+        )
+    }
+    return <MediaEmbed mediaUrl={mediaUrl} mediaType={mediaType} />
+}
+
 type Props = {
     feedbackRequestSummary: FeedbackRequestSummaryProps,
 };
 
-class FeedbackRequestSummaryContent extends React.Component<Props> {
-    /*
-     * Component for inline feedback request preview shown on FeedbackGroupsPage and in FeedbackRequestForm
-     */
-
-    renderMedia = () => {
-        if (!this.props.feedbackRequestSummary.mediaUrl) {
-            return (
-                <Alert
-                    message="No track has been provided for this request. You'll be able to join a group and provide feedback for others, but you won't receive any feedback for yourself."
-                    type="info"
-                    showIcon
+/*
+ * Component for inline feedback request preview shown on FeedbackGroupsPage and in FeedbackRequestForm
+ */
+const FeedbackRequestSummaryContent = ({ feedbackRequestSummary }: Props) => (
+    <div>
+        <Row gutter={[16, 16]}>
+            <Col>
+                <Media
+                    mediaUrl={feedbackRequestSummary.mediaUrl}
+                    mediaType={feedbackRequestSummary.mediaType}
                 />
-            )
-        }
-        if (!this.props.feedbackRequestSummary.mediaType) {
-            return (
-                <Alert
-                    message="Unable to play media. Invalid URL."
-                    type="error"
-                    showIcon
-                />
-            )
-        }
-        return <MediaEmbed mediaUrl={this.props.feedbackRequestSummary.mediaUrl} mediaType={this.props.feedbackRequestSummary.mediaType} />
-    };
-    
-    render() {
-        
-        return (
-            <div>
-                <Row gutter={[16, 16]}>
-                    <Col>
-                        {this.renderMedia()}
-                    </Col>
-                </Row>
-                <Row gutter={[16, 16]}>
-                    <Col>
-                        <Typography.Text strong>Genre: </Typography.Text>
-                        <Typography.Text>{GENRE_OPTIONS[this.props.feedbackRequestSummary.genre]}</Typography.Text>
-                    </Col>
-                </Row>
-                {this.props.feedbackRequestSummary.mediaUrl && <Row>
-                    {this.props.feedbackRequestSummary.feedbackPrompt && <Col>
-                        <Typography.Text strong>You said: </Typography.Text>
-                        <Typography.Text>"{this.props.feedbackRequestSummary.feedbackPrompt}"</Typography.Text>
-                    </Col>}
-                    {!this.props.feedbackRequestSummary.feedbackPrompt && <Col>
-                        <Typography.Text strong>You did not provide any additional information for feedback.</Typography.Text>
-                    </Col>}
-                </Row>}
-            </div>
-        );
-    }
-}
+            </Col>
+        </Row>
+        <Row gutter={[16, 16]}>
+            <Col>
+                <Typography.Text strong>Genre: </Typography.Text>
+                <Typography.Text>{GENRE_OPTIONS[feedbackRequestSummary.genre]}</Typography.Text>
+            </Col>
+        </Row>
+        {feedbackRequestSummary.mediaUrl && <Row>
+            {feedbackRequestSummary.feedbackPrompt && <Col>
+                <Typography.Text strong>You said: </Typography.Text>
+                <Typography.Text>"{feedbackRequestSummary.feedbackPrompt}"</Typography.Text>
+            </Col>}
+            {!feedbackRequestSummary.feedbackPrompt && <Col>
+                <Typography.Text strong>You did not provide any additional information for feedback.</Typography.Text>
+            </Col>}
+        </Row>}
+    </div>
+);
 
 export default FeedbackRequestSummaryContent;
