0623bfbef4b127a31cc27f88653262e96bf90d13
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-09-29 14:52:55 -0700

    Login and registration forms use useMutation.

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,15 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
+import { useMutation } from 'react-query';
 
 import apiRoot from '../../apiRoot';
 
 import { Alert, Button, Col, Icon, Input, Form, Result, Row, Spin } from 'antd';
-
-type State = {
-    requestSent: boolean,
-    errorMessage: string,
-    submitted: boolean,
-};
 
 const TOKEN_AUTH_MUTATION = `mutation TokenAuth($username: String!, $password: String!) {
   tokenAuth(username: $username, password: $password) {
@@ -19,32 +14,23 @@ const TOKEN_AUTH_MUTATION = `mutation TokenAuth($username: String!, $password: S
 
 const GA_LOGIN_CATEGORY = "login";
 
-class UnwrappedLoginForm extends React.Component<Props, State> {
+const UnwrappedLoginForm = ({ form }) => {
     /*
      * Component for displaying login form.
      */
-    state = {
-        requestSent: false,
-        errorMessage: null,
-        submitted: false,
-    };
-
-    componentDidMount() {
+    useEffect(() => {
         ReactGA.event({
             category: GA_LOGIN_CATEGORY,
             action: "view",
         });
-    }
+    }, []);
     
-    submitForm = (username, password) => {
-        this.setState({
-            requestSent: true,
-        })
+    const submitForm = async ({ username, password }) => {
         ReactGA.event({
             category: GA_LOGIN_CATEGORY,
             action: "submit",
         });
-        return fetch(apiRoot +'/graphql/', {
+        const result = await fetch(apiRoot + '/graphql/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -55,105 +41,98 @@ class UnwrappedLoginForm extends React.Component<Props, State> {
                 variables: { username, password },
             }),
             credentials: 'include',
-        }).then(result =>
-            result.json()
-        ).then((data) => {
-            if (data['errors']) {
-                ReactGA.event({
-                    category: GA_LOGIN_CATEGORY,
-                    action: "error",
-                });
-                this.setState({
-                    requestSent: false,
-                    submitted: false,
-                    errorMessage: data['errors'][0]['message'],
-                });
-            } else {
-                ReactGA.event({
-                    category: GA_LOGIN_CATEGORY,
-                    action: "success",
-                });
-                this.setState({
-                    requestSent: false,
-                    submitted: true,
-                    errorMessage: null,
-                });
-                window.location.reload();
-            }
         });
+        return await result.json();
     };
 
-    onSubmit = (event) => {
-        event.preventDefault();
-        this.props.form.validateFieldsAndScroll((err, values) => {
-            if (!err) {
-                this.submitForm(
-                    values.username,
-                    values.password
-                )
-            }
-        });
-    };
+    const [submitFormMutate, { isLoading, data }] = useMutation(submitForm);
 
-    render() {
-        return (
-            <div>
-                <Row gutter={[16, 16]}>
-                    <Col>
-                        {this.state.errorMessage && <Alert message={this.state.errorMessage} type="error" showIcon />}
-                    </Col>
-                </Row>
-                <Row gutter={[16, 16]}>
-                    <Col>
-                        {!this.state.submitted && <Spin spinning={this.state.requestSent}>
-                            <Form onSubmit={this.onSubmit} className="hmt-form">
-                                <Form.Item>
-                                    {this.props.form.getFieldDecorator('username', {
-                                        rules: [{ required: true, message: 'Please enter your username.' }],
-                                    })(
-                                        <Input
-                                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                            placeholder="Email"
-                                            autoFocus
-                                        />,
-                                    )}
-                                </Form.Item>
-                                <Form.Item>
-                                    {this.props.form.getFieldDecorator('password', {
-                                        rules: [{ required: true, message: 'Please enter your password.' }],
-                                    })(
-                                        <Input
-                                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                            type="password"
-                                            placeholder="Password"
-                                        />,
-                                    )}
-                                </Form.Item>
-                                <Form.Item>
-                                    <Button
-                                        block
-                                        type="primary"
-                                        htmlType="submit"
-                                        disabled={this.state.submitted}
-                                    >
-                                        Sign In
-                                    </Button>
-                                    <a href={apiRoot + '/accounts/password_reset'} style={{ float: 'right'}}>
-                                        Forgot password?
-                                    </a>
-                                </Form.Item>
-                            </Form>
-                        </Spin>}
-                        {this.state.submitted && <Result
-                            status="success"
-                            title="You have been signed in."
-                            subTitle="Please wait to be redirected."
-                        />}
-                    </Col>
-                </Row>
-            </div>
-        );
+    if (data) {
+        if (data.errors) {
+            ReactGA.event({
+                category: GA_LOGIN_CATEGORY,
+                action: "error",
+            });
+        } else {
+            ReactGA.event({
+                category: GA_LOGIN_CATEGORY,
+                action: "success",
+            });
+            window.location.reload();
+        }
     }
+
+    const submitted = (data && !data.errors);
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                submitFormMutate({
+                    username: values.username,
+                    password: values.password,
+                })
+            }
+        });
+    };
+
+    return (
+        <div>
+            <Row gutter={[16, 16]}>
+                <Col>
+                    {data && data.error && <Alert message={data.error} type="error" showIcon />}
+                </Col>
+            </Row>
+            <Row gutter={[16, 16]}>
+                <Col>
+                    {!submitted && <Spin spinning={isLoading}>
+                        <Form onSubmit={onSubmit} className="hmt-form">
+                            <Form.Item>
+                                {form.getFieldDecorator('username', {
+                                    rules: [{ required: true, message: 'Please enter your username.' }],
+                                })(
+                                    <Input
+                                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                        placeholder="Email"
+                                        autoFocus
+                                    />,
+                                )}
+                            </Form.Item>
+                            <Form.Item>
+                                {form.getFieldDecorator('password', {
+                                    rules: [{ required: true, message: 'Please enter your password.' }],
+                                })(
+                                    <Input
+                                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                        type="password"
+                                        placeholder="Password"
+                                    />,
+                                )}
+                            </Form.Item>
+                            <Form.Item>
+                                <Button
+                                    block
+                                    type="primary"
+                                    htmlType="submit"
+                                    disabled={submitted}
+                                >
+                                    Sign In
+                                </Button>
+                                <a href={apiRoot + '/accounts/password_reset'} style={{ float: 'right'}}>
+                                    Forgot password?
+                                </a>
+                            </Form.Item>
+                        </Form>
+                    </Spin>}
+                    {submitted && <Result
+                        status="success"
+                        title="You have been signed in."
+                        subTitle="Please wait to be redirected."
+                    />}
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
 export default Form.create({ name: 'login' })(UnwrappedLoginForm);
