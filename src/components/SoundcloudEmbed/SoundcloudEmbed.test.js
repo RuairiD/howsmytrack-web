d19d26d88ad9330c24169f99809d53cfef1dbc72
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-10-15 23:15:18 -0700

    Added SoundcloudEmbed test for secret links.

--- src/components/SoundcloudEmbed/SoundcloudEmbed.test.js
+++ src/components/SoundcloudEmbed/SoundcloudEmbed.test.js
@@ -16,6 +16,19 @@ describe("SoundcloudEmbed", () => {
         expect(wrapper.find("Iframe").get(0).props.height).toBe(20);
     });
 
+    it("renders with a formatted secret. mediaUrl", () => {
+        const wrapper = mount(
+            <SoundcloudEmbed
+                mediaUrl="https://soundcloud.com/ruairidx/bruno/s-WTUMOLA0Bsk"
+                size="small"
+            />,
+        );
+        expect(wrapper.find("Iframe").get(0).props.src).toBe(
+            "https://w.soundcloud.com/player/?url=https://soundcloud.com/ruairidx/bruno/?secret_token=s-WTUMOLA0Bsk&color=%23ff5500&inverse=false&auto_play=false&show_user=true",
+        );
+        expect(wrapper.find("Iframe").get(0).props.height).toBe(20);
+    });
+
     it("renders a large player", () => {
         const wrapper = mount(
             <SoundcloudEmbed
