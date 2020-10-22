52424de346c73d03fba35b8a3b7c4394d79a9e8d
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-10-22 10:33:06 -0700

    Removed TransparentText hack, fixed with CSS (finally).

--- src/pages/GenericPage/GenericPage.js
+++ src/pages/GenericPage/GenericPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 
-import { Layout, PageHeader, Typography } from "antd";
+import { Layout, PageHeader } from "antd";
 import { Div } from "lemon-reset";
 import axios from "axios";
 import apiRoot from "../../apiRoot";
@@ -20,14 +20,6 @@ type Props = {
     hideMenu: boolean,
     isMobile: boolean,
 };
-
-// TODO: I hate this but it was a quick and dirty way of making sure the page filled the container and
-// stayed the same width as other pages which had text they actually needed.
-const TransparentText = () => (
-    <Typography.Paragraph style={{ opacity: 0, height: 0, width: "100%", userSelect: "none" }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed hendrerit leo. Donec vitae risus et ante egestas sollicitudin at a mi. Duis fringilla a mi ut congue. Sed elit nunc, mollis sit amet interdum id, viverra vitae ligula. Proin eu risus vitae turpis fermentum maximus. Phasellus finibus enim nibh, non cursus lorem auctor interdum. Sed a ex id magna ultricies gravida. Proin sit amet sem at quam tristique tristique sit amet sed augue. Cras in bibendum risus, eu consequat turpis. In sed commodo augue. Donec nibh nulla, viverra quis mi tincidunt, pulvinar interdum neque.
-    </Typography.Paragraph>
-);
 
 const GenericPage = ({ title, subTitle, hideMenu, isMobile, children }: Props) => {
     /*
@@ -60,7 +52,6 @@ const GenericPage = ({ title, subTitle, hideMenu, isMobile, children }: Props) =
                             <Layout className="content">
                                 <Layout.Content>
                                     {children}
-                                    <TransparentText />
                                 </Layout.Content>
                                 <Layout.Footer>
                                     <PageFooter />
