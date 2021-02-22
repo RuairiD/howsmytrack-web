b0edb5ea429fba0aacef599ebf973fc873c59c36
Ruairi Dorrity <ruairidorrity@gmail.com>
2021-02-22 10:10:12 -0800

    Replaced express-force-https with handrolled solution.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
-const expressForceHttps = require("express-force-https");
 
 const BUILD_PATH = path.join(__dirname, "..", "build");
 const PORT = process.env.PORT || 5000;
@@ -10,7 +9,14 @@ const app = express();
 
 app.use(express.static(BUILD_PATH));
 app.use(morgan("combined"));
-app.use(expressForceHttps);
+// Force HTTPS redirection for HTTP requests.
+app.use((req, res, next) => {
+    if (req.secure) {
+        next();
+    } else {
+        res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+});
 
 app.get("*", (_req, res) => {
     res.sendFile(path.join(BUILD_PATH, "index.html"));
