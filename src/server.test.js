2d77cbc61776357e98ddb318e8b1e2c1c798fe19
Ruairi Dorrity <ruairidorrity@gmail.com>
2021-02-22 12:51:57 -0800

    Close server connection after testing.

--- src/server.test.js
+++ src/server.test.js
@@ -2,6 +2,11 @@ const request = require("supertest");
 const server = require("./server");
 
 describe("loading express", () => {
+    afterAll((done) => {
+        server.close();
+        done();
+    });
+
     it("handles / requests", (done) => {
         request(server).get("/").set("x-forwarded-proto", "https").expect(200, done);
     });
